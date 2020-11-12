ecd5be13b59c7f44cbeb62199849367c063aa2ea
buryk20 <73736907+buryk20@users.noreply.github.com>
2020-11-12 13:50:42 +0200

    v0.1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var F = prompt("введите значение силы");
+var M = prompt("введите значение массы");
+var a = ( F / M );
+
+document.getElementById("F").innerHTML = F;
+document.getElementById("M").innerHTML = M;
+document.getElementById("a").innerHTML = a;
